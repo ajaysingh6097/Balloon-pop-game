a5856bef8042a28f69e0f9550656106d7987ba30
ajaysingh6097 <ajaysingh60970@gmail.com>
2026-07-11 12:46:17 +0530

    added .github

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: 'Balloon-pop-game',
+    base: '/Balloon-pop-game/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
